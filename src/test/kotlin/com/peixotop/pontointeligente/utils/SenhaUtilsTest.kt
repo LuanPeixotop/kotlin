57afb6cf32479d8f5201bb4c8cdc5c5334538c8f
LuanPeixotop <luanpeixotopereira@gmail.com>
2020-01-05 19:02:27 -0300

    add senhaUtils to generate Bcrypt

---
+++ src/test/kotlin/com/peixotop/pontointeligente/utils/SenhaUtilsTest.kt
@@ -0,0 +1,16 @@
+package com.peixotop.pontointeligente.utils
+
+import org.junit.jupiter.api.Assertions
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.junit.jupiter.api.Test
+
+class SenhaUtilsTest {
+    private val SENHA = "SenhaFoda"
+    private val bCryptEncoder = BCryptPasswordEncoder()
+
+    @Test
+    fun testGerarBcrypt() {
+        val bcrypt = SenhaUtils().gerarBcrypt(SENHA)
+        Assertions.assertTrue(bCryptEncoder.matches(SENHA, bcrypt))
+    }
+}
