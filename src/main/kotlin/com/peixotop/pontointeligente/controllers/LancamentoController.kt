0b5384fb116b8adfb7c25bffbc84ad9777326f1e
LuanPeixotop <luanpeixotopereira@gmail.com>
2020-01-25 20:48:11 -0300

    feat: add lancamentos controller

---
+++ src/main/kotlin/com/peixotop/pontointeligente/controllers/LancamentoController.kt
@@ -0,0 +1,206 @@
+package com.peixotop.pontointeligente.controllers
+
+import com.peixotop.pontointeligente.Response
+import com.peixotop.pontointeligente.documents.Funcionario
+import com.peixotop.pontointeligente.documents.Lancamento
+import com.peixotop.pontointeligente.dtos.LancamentoDto
+import com.peixotop.pontointeligente.enums.TipoEnum
+import com.peixotop.pontointeligente.services.FuncionarioService
+import com.peixotop.pontointeligente.services.LancamentoService
+import org.apache.commons.lang3.EnumUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/lancamentos")
+class LancamentoController(val lancamentoService: LancamentoService,
+                           val funcionarioService: FuncionarioService) {
+
+    private val dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @Value("\${paginacao.qtd_por_pagina}")
+    val qtdPorPagina: Int = 0
+
+    @PostMapping
+    fun adicionar(@Valid @RequestBody lancamentoDto: LancamentoDto,
+                  result: BindingResult): ResponseEntity<Response<LancamentoDto>> {
+
+        val response: Response<LancamentoDto> = Response<LancamentoDto>()
+
+        validarFuncionario(lancamentoDto, result)
+        validaTipoLancamento(lancamentoDto.tipo, result)
+        validaDataLancamento(lancamentoDto.data, result)
+
+        if (result.hasErrors()) {
+            for (error in result.allErrors) error.defaultMessage?.let { response.errors.add(it) }
+            return ResponseEntity.badRequest().body(response)
+        }
+
+        val lancamento: Lancamento =
+                lancamentoService.persistir(converterDtoParaLancamento(lancamentoDto, result))
+
+        response.data = converterLancamentoParaDto(lancamento)
+        return ResponseEntity.ok(response)
+    }
+
+    @PutMapping(value = ["/{id}"])
+    fun atualizar(@PathVariable("id") id: String,
+                  @Valid @RequestBody lancamentoDto: LancamentoDto,
+                  result: BindingResult):
+            ResponseEntity<Response<LancamentoDto>> {
+
+        val response: Response<LancamentoDto> = Response<LancamentoDto>()
+
+        val lancamento: Lancamento? = lancamentoService.buscarPorId(id)
+
+        if (lancamento == null) result.addError(ObjectError("lancamento",
+                "O Lancamento de id $id não foi encontrado."))
+
+        if (lancamentoDto.id != id) result.addError(ObjectError("lancamento",
+                "O ID do objeto enviado (${lancamentoDto.id}) é diferente do ID passado como parâmetro ($id)"))
+
+        validarFuncionario(lancamentoDto, result)
+        validaTipoLancamento(lancamentoDto.tipo, result)
+        validaDataLancamento(lancamentoDto.data, result)
+
+        if (result.hasErrors()) {
+            for (error in result.allErrors) error.defaultMessage?.let { response.errors.add(it) }
+            return ResponseEntity.badRequest().body(response)
+        }
+
+        var lancamentoAtualizado: Lancamento = converterDtoParaLancamento(lancamentoDto, result)
+        lancamentoAtualizado = lancamentoService.persistir(lancamentoAtualizado)
+
+        response.data = converterLancamentoParaDto(lancamentoAtualizado)
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping(value = ["/{id}"])
+    private fun listarPorId(@PathVariable("id") id: String): ResponseEntity<Response<LancamentoDto>> {
+        val response: Response<LancamentoDto> = Response<LancamentoDto>()
+        val lancamento: Lancamento? = lancamentoService.buscarPorId(id)
+
+        if (lancamento == null) {
+            response.errors.add("Lancamento não encontrado para o ID: $id")
+            return ResponseEntity.badRequest().body(response)
+        }
+
+        response.data = converterLancamentoParaDto(lancamento)
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping(value = ["/funcionario/{funcionarioId}"])
+    fun listarPorfuncionarioId(@PathVariable("funcionarioId") funcionarioId: String,
+                               @RequestParam(value = "pag", defaultValue = "0") pag: Int,
+                               @RequestParam(value = "ord", defaultValue = "id") ord: String,
+                               @RequestParam(value = "dir", defaultValue = "DESC") dir: String):
+            ResponseEntity<Response<Page<LancamentoDto>>> {
+
+        val response: Response<Page<LancamentoDto>> = Response<Page<LancamentoDto>>()
+
+        val direction: String = dir.toUpperCase()
+
+        if (!EnumUtils.isValidEnum(Sort.Direction::class.java, direction)) {
+            response.errors.add("Tipo de direção não suportado. Tipos Suportados: " +
+                    Sort.Direction.values().joinToString())
+            return ResponseEntity.badRequest().body(response)
+        }
+
+        val pageRequest: PageRequest = PageRequest.of(pag, qtdPorPagina, Sort.Direction.valueOf(direction), ord)
+        val lancamentos: Page<Lancamento> = lancamentoService.buscarPorFuncionarioId(funcionarioId, pageRequest)
+
+        val lancamentoDto: Page<LancamentoDto> =
+                lancamentos.map { lancamento -> converterLancamentoParaDto(lancamento) }
+
+        response.data = lancamentoDto
+
+        return ResponseEntity.ok(response)
+    }
+
+    @DeleteMapping(value = ["/{id}"])
+    fun remover(@PathVariable("id") id: String) : ResponseEntity<Response<String>> {
+        val response: Response<String> = Response<String>()
+        val lancamento: Lancamento? = lancamentoService.buscarPorId(id)
+
+        if (lancamento == null) {
+            response.errors.add("Erro ao remover o lançamento $id. Registro não encontrado")
+            ResponseEntity.badRequest().body(response)
+        }
+
+        lancamentoService.remover(id)
+        response.data = "O lançamento $id foi removido com sucesso."
+        return ResponseEntity.ok(response)
+    }
+
+
+    private fun converterDtoParaLancamento(lancamentoDto: LancamentoDto, result: BindingResult): Lancamento {
+        if (lancamentoDto.id != null) {
+            val lancamento: Lancamento? = lancamentoService.buscarPorId(lancamentoDto.id)
+            if (lancamento == null) result.addError(ObjectError("lancamento",
+                    "Lançamento não encontrado."))
+        }
+
+        return Lancamento(
+                LocalDateTime.parse(lancamentoDto.data, dateTimeFormatter),
+                TipoEnum.valueOf(lancamentoDto.tipo),
+                lancamentoDto.funcionarioId!!,
+                lancamentoDto.descricao,
+                lancamentoDto.localizacao,
+                lancamentoDto.id
+        )
+    }
+
+    private fun converterLancamentoParaDto(lancamento: Lancamento?): LancamentoDto =
+            LancamentoDto(
+                    lancamento?.data.toString(),
+                    lancamento?.tipo.toString(),
+                    lancamento?.funcionarioId,
+                    lancamento?.descricao,
+                    lancamento?.localizacao,
+                    lancamento?.id
+            )
+
+    private fun validarFuncionario(lancamentoDto: LancamentoDto, result: BindingResult) {
+        if (lancamentoDto.funcionarioId.isNullOrBlank()) {
+            result.addError(ObjectError("funcionario",
+                    "Funcionário Não Informado."))
+            return
+        }
+
+        val funcionario: Funcionario? = funcionarioService.buscarPorId(lancamentoDto.funcionarioId)
+
+        if (funcionario == null) {
+            result.addError(ObjectError("funcionario",
+                    "Funcionário não encontrado."))
+        }
+    }
+
+    private fun validaTipoLancamento(valorEnum: String,
+                                     result: BindingResult) {
+        if (!EnumUtils.isValidEnum(TipoEnum::class.java,
+                        valorEnum)) {
+            result.addError(ObjectError("lancamento",
+                    "Tipo de lancamento Não existente. Tipos Suportados: " +
+                            TipoEnum.values().joinToString()))
+        }
+    }
+
+    private fun validaDataLancamento(data: String?, result: BindingResult) {
+        try {
+            LocalDateTime.parse(data, dateTimeFormatter)
+        } catch (e: DateTimeParseException) {
+            result.addError(ObjectError("lancamento",
+                    "Data do lançamento fora do padrão ISO_LOCAL_DATE_TIME"))
+        }
+    }
+}
---
+++ src/test/kotlin/com/peixotop/pontointeligente/controllers/LancamentoControllerTest.kt
@@ -0,0 +1,124 @@
+package com.peixotop.pontointeligente.controllers
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.peixotop.pontointeligente.documents.Funcionario
+import com.peixotop.pontointeligente.documents.Lancamento
+import com.peixotop.pontointeligente.dtos.LancamentoDto
+import com.peixotop.pontointeligente.enums.PerfilEnum
+import com.peixotop.pontointeligente.enums.TipoEnum
+import com.peixotop.pontointeligente.services.FuncionarioService
+import com.peixotop.pontointeligente.services.LancamentoService
+import com.peixotop.pontointeligente.utils.SenhaUtils
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.LocalDateTime
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@WithMockUser
+@AutoConfigureMockMvc
+class LancamentoControllerTest {
+
+    @Autowired
+    private val mvc: MockMvc? = null
+    @MockBean
+    private val lancamentoService: LancamentoService? = null
+    @MockBean
+    private val funcionarioService: FuncionarioService? = null
+
+    private val urlBase: String = "/api/lancamentos/"
+    private val funcionarioId: String = "1"
+    private val lancamentoId: String = "1"
+    private val tipo: String = TipoEnum.INICIO_TRABALHO.name
+    private val data: LocalDateTime = LocalDateTime.now()
+    private val descricao = "descrição"
+    private val localizacao = "1.234.567.890"
+
+    @Test
+    @Throws(Exception::class)
+    fun testCadastrarLancamento() {
+        val lancamento: Lancamento = obterDadosLancamento()
+        BDDMockito.given<Funcionario>(funcionarioService?.buscarPorId(funcionarioId)).willReturn(funcionario())
+        BDDMockito.given(lancamentoService?.persistir(obterDadosLancamento())).willReturn(lancamento)
+        mvc!!.perform(MockMvcRequestBuilders.post(urlBase)
+                .content(obterJsonRequisicaoPost())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.errors").isEmpty)
+                .andExpect(jsonPath("$.data.tipo").value(tipo))
+                .andExpect(jsonPath("$.data.data").value(data.toString()))
+                .andExpect(jsonPath("$.data.funcionarioId").value(funcionarioId))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testCadastrarLancamentoFuncionarioInvalido() {
+        BDDMockito.given<Funcionario>(funcionarioService?.buscarPorId(funcionarioId)).willReturn(null)
+        mvc!!.perform(MockMvcRequestBuilders.post(urlBase)
+                .content(obterJsonRequisicaoPost())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest)
+                .andExpect(jsonPath("$.errors").value("Funcionário não encontrado."))
+                .andExpect(jsonPath("$.data").isEmpty)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testRemoverLancamento() {
+        BDDMockito.given<Lancamento>(lancamentoService?.buscarPorId(lancamentoId)).willReturn(obterDadosLancamento())
+
+        mvc!!.perform(MockMvcRequestBuilders.delete(urlBase + funcionarioId)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.data")
+                        .value("O lançamento $lancamentoId foi removido com sucesso."))
+    }
+
+    private fun obterDadosLancamento(): Lancamento = Lancamento(
+            data,
+            TipoEnum.valueOf(tipo),
+            funcionarioId,
+            descricao,
+            localizacao,
+            lancamentoId
+    )
+
+    private fun funcionario(): Funcionario =
+            Funcionario(
+                    "nome",
+                    "email@email.com",
+                    SenhaUtils().gerarBcrypt("senha"),
+                    "12345678909",
+                    PerfilEnum.ROLE_USUARIO,
+                    "1"
+            )
+
+    @Throws(JsonProcessingException::class)
+    private fun obterJsonRequisicaoPost(): String {
+        val lancamentoDto = LancamentoDto(
+                data.toString(),
+                tipo,
+                funcionarioId,
+                descricao,
+                localizacao,
+                funcionarioId
+        )
+        val mapper = ObjectMapper()
+        return mapper.writeValueAsString(lancamentoDto)
+    }
+}
